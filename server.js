82d1d02c6ea4bebf67fe7de87896ed4058f2080f
Nino Vrijman <ninovrijman@gmail.com>
2017-05-22 14:07:22 +0200

    Node back-end done

--- server.js
+++ server.js
@@ -1,41 +1,78 @@
 const Rx = require('rxjs/Rx');
-
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongojs = require('mongojs');
 
 const kweet_app = express();
-const bodyParser = require('body-parser');
 kweet_app.use(bodyParser());
 
-// MongoDB
-const mongojs = require('mongojs');
-var db = mongojs('mongodb://127.0.0.1:27017/kwetter', ['kweets', 'users']);
+var database = mongojs('mongodb://127.0.0.1:27017/kwetter', ['kweets', 'users']);
 
-kweet_app.post('/kweets/new', (req, res, next) => {
-    const kweet = req.body;
+kweet_app.get('/users/:username', (req, res) => {
+    const username = req.params.username;
 
-    db.kweets.save(kweet, (error, newKweet) => {
-        if (error) {
-            res.send(err);
-        } else {
-            res.json(newKweet);
-        }
+    getUserByUsername(username).subscribe((result) => {
+        res.send(result);
     });
 });
 
-kweet_app.get('/kweets/timeline', (req, res, next) => {
-    console.log("timeline kweets");
+const getUserByUsername = (username) => Rx.Observable.fromPromise(new Promise((resolve, reject) => {
+    database.users.findOne({ username: username }, (error, user) => {
+        if (error) {
+            resolve(error);
+        }
+        resolve(user);
+    });
+}));
+
+kweet_app.post('/kweets/new', (req, res, next) => {
+    const kweet = req.body.body ? req.body.body : req.body;
+
+    createKweet(kweet).subscribe((createdKweet) => {
+        res.send(createdKweet);
+    });
 });
 
-kweet_app.get('/kweets/profile', (req, res, next) => {
-    console.log("profile kweets");
+const createKweet = (kweet) => Rx.Observable.fromPromise(new Promise((resolve, reject) => {
+    database.kweets.save(kweet, (error, createdKweet) => {
+        if (error) {
+            resolve(error);
+        }
+        resolve(createdKweet);
+    });
+}));
+
+kweet_app.get('/kweets/timeline/:userId/:offset/:amount', (req, res, next) => {
+    const userId = req.params.userId;
+
+    getKweets(userId).subscribe(timelineKweets => {
+        res.send(timelineKweets);
+    });
 });
+
+kweet_app.get('/kweets/profile/:userId/:offset/:amount', (req, res, next) => {
+    const userId = req.params.userId;
+
+    getKweets(userId).subscribe(userKweets => {
+        res.send(userKweets);
+    });
+});
+
+const getKweets = (userId) => Rx.Observable.fromPromise(new Promise((resolve, reject) => {
+    database.kweets.find({ creatorId: Number(userId) }, (error, kweets) => {
+        if (error) {
+            resolve(error);
+        }
+        resolve(kweets);
+    });
+}));
 
 const getFollowingUserIDs = (userId) => Rx.Observable.fromPromise(new Promise((resolve, reject) => {
-    db.users.findOne({ _id: mongojs.ObjectId(userId) }, (err, user) => {
-        if (err) {
-            resolve(err);
+    database.users.findOne({ _id: mongojs.ObjectId(userId) }, (error, user) => {
+        if (error) {
+            resolve(error);
         }
-        resolve(JSON.parse(user));
+        resolve(user);
     });
 }));
 
